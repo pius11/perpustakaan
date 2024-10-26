c85040e749141b9071b962e37d46d1dc07e4cf1f
pius11 <pius17431@gmail.com>
2024-10-26 18:56:13 +0800

    m

--- app/view/template/header.php
+++ app/view/template/header.php
@@ -12,7 +12,7 @@
   
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">PERPUS PNB</a>
+  <a class="navbar-brand" href="<?=BASEURL?>">PERPUS PNB</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
